2b4922edd96046ac87898e1716c6499d52862d56
Thomas Koshy <ttkoshy@gmail.com>
2021-09-16 16:52:58 -0400

    initial commit

---
+++ src/FirstBank.java
@@ -0,0 +1,11 @@
+public class FirstBank {
+    public static void main(String[] args) {
+
+        Account tom = new Account("Thomas Koshy", "A00001");
+        tom.showMenu();
+
+        Account phil = new Account("Philip Koshy", "B00002");
+        phil.showMenu();
+
+    }
+}
